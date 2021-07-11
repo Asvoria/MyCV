57cd925c56b110087a8bb422b21ab40b1da94d13
ThriftyOldStudent <asvoria@live.com>
2021-07-11 14:52:53 +0800

    e

--- button.js
+++ button.js
@@ -2,13 +2,13 @@ var btn = document.getElementById('btnup');
 
 $(window).scroll(function() {
   if ($(window).scrollTop() > 200) {
-    btn.addClass('show');
+    $(btn).addClass('show');
   } else {
-    btn.removeClass('visible');
+    $(btn).removeClass('show');
   }
 });
 
-btn.click(function(e) {
+$(btn).click(function(e) {
   e.preventDefault();
   $('html, body').animate({scrollTop:0}, '200');
 });
